af9d8e3721ddc17b6c4e76511cf6a1aea1b65a6d
ramu-nukavarapu <ramunukavarapu2004@gmail.com>
2025-09-27 23:56:01 +0530

    feat: db initialization
    Signed-off-by: ramu-nukavarapu <ramunukavarapu2004@gmail.com>

--- main.go
+++ main.go
@@ -6,9 +6,14 @@ import (
 )
 
 func main() {
+	// Initialize the DB
+	db := InitializeDB()
+
+	// Create the Fiber App Instance
 	app := fiber.New(fiber.Config{
 		AppName: "Library API",
 	})
 
+	// Start the server
 	log.Fatal(app.Listen(":3000"))
 }
