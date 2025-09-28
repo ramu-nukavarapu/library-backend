108d058b9fcc4a2b7fd1d3580f610673ba9574d9
ramu-nukavarapu <ramunukavarapu2004@gmail.com>
2025-09-28 17:21:12 +0530

    feat: add get route to retrieve all books
    Signed-off-by: ramu-nukavarapu <ramunukavarapu2004@gmail.com>

--- book.go
+++ book.go
@@ -24,6 +24,41 @@ func BookHandlers(route fiber.Router, db *gorm.DB) {
 		return c.Status(fiber.StatusCreated).JSON(book)
 	})
 
+	route.Get("/", func(c *fiber.Ctx) error {
+		userId := c.Locals("userId").(int)
+		books := new([]Book)
+
+		title := c.Query("title")
+		status := c.Query("status")
+		author := c.Query("author")
+		year := c.QueryInt("year")
+
+		query := db.Where("userId = ?", userId)
+
+		if title != "" {
+			query.Where("title LIKE ?", "%"+title+"%")
+		}
+
+		if status != "" {
+			query.Where("status = ?", status)
+		}
+
+		if author != "" {
+			query.Where("author = ?", author)
+		}
+
+		if year != 0 {
+			query.Where("year = ?", year)
+		}
+
+		if err := query.Find(&books).Error; err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "book not found.",
+			})
+		}
+		return c.Status(fiber.StatusOK).JSON(books)
+	})
+
 	route.Get("/:id", func(c *fiber.Ctx) error {
 		bookId, err := c.ParamsInt("id")
 		if err != nil {
@@ -36,7 +71,7 @@ func BookHandlers(route fiber.Router, db *gorm.DB) {
 
 		if err := db.Where("id = ? AND userId = ?", bookId, userId).First(&book).Error; err != nil {
 			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-				"error": err.Error(),
+				"error": "book not found.",
 			})
 		}
 		return c.Status(fiber.StatusOK).JSON(book)
