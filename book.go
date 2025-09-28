73f0a153f7ab38b09dec68dd0f442dbea042404f
ramu-nukavarapu <ramunukavarapu2004@gmail.com>
2025-09-28 17:08:32 +0530

    feat: add get route to retrieve a book by id
    Signed-off-by: ramu-nukavarapu <ramunukavarapu2004@gmail.com>

--- book.go
+++ book.go
@@ -24,4 +24,21 @@ func BookHandlers(route fiber.Router, db *gorm.DB) {
 		return c.Status(fiber.StatusCreated).JSON(book)
 	})
 
+	route.Get("/:id", func(c *fiber.Ctx) error {
+		bookId, err := c.ParamsInt("id")
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		userId := c.Locals("userId").(int)
+		book := new(Book)
+
+		if err := db.Where("id = ? AND userId = ?", bookId, userId).First(&book).Error; err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		return c.Status(fiber.StatusOK).JSON(book)
+	})
 }
