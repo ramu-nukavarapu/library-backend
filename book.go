85417cfa8ab2f5c8a25d99c363db49c636668c02
ramu-nukavarapu <ramunukavarapu2004@gmail.com>
2025-09-28 17:07:58 +0530

    feat: add post route to add books
    Signed-off-by: ramu-nukavarapu <ramunukavarapu2004@gmail.com>

--- book.go
+++ book.go
@@ -6,5 +6,22 @@ import (
 )
 
 func BookHandlers(route fiber.Router, db *gorm.DB) {
+	route.Post("/", func(c *fiber.Ctx) error {
+		book := new(Book)
+		book.UserID = c.Locals("userId").(int)
+
+		if err := c.BodyParser(book); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		if err := db.Create(&book).Error; err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		return c.Status(fiber.StatusCreated).JSON(book)
+	})
 
 }
